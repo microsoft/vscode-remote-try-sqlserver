cd29315791f8885cf097048cd6da01b2e04e16c9
Christopher MANEU <chmaneu@microsoft.com>
2022-09-22 14:30:15 +0000

    Add SQLCMD demo script

---
+++ 01-CreateDatabase.sql
@@ -0,0 +1,12 @@
+-- Create a new database called 'DatabaseName'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'TrySQLServer'
+)
+CREATE DATABASE TrySQLServer
+GO
