066a7753c0ec8c7754f665bfb105ace0b454d076
Christopher MANEU <chmaneu@microsoft.com>
2022-09-22 14:30:27 +0000

    Add Database project

---
+++ TrySqlInContainer/Users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Users]
+(
+  [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(),
+  [FirstName] NVARCHAR(100) NOT NULL,
+  [LastName] NVARCHAR(100),
+  [Email] NVARCHAR(200)
+)
